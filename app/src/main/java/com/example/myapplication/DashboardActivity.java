59b3398e8b121f14ffe3dc37a8ec689a30288dfd
Neelrayal <neelrayalsonu@gmail.com>
2022-01-17 15:01:33 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    EditText secretCodeBox;
+    Button joinBtn, shareBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        secretCodeBox = findViewById(R.id.codeBox);
+        joinBtn = findViewById(R.id.joinBtn);
+        shareBtn = findViewById(R.id.shareBtn);
+
+        try {
+            URL serverURL = new URL("https://meet.jit.si");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
